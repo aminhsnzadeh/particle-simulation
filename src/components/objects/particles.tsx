9f4be1e48b353a71590d4c53683c3220187712fe
amin.hasanzadeh <amin.hasanzadeh@axon.teh>
2025-08-18 13:14:36 +0330

    Added rotation animation

--- src/components/objects/particles.tsx
+++ src/components/objects/particles.tsx
@@ -1,5 +1,8 @@
 import type {particleModelType} from "../../types/particle.ts";
 import {useTexture} from "@react-three/drei";
+import {useFrame} from "@react-three/fiber";
+import {useRef} from "react";
+import * as THREE from "three";
 
 
 export default function Particles({ particleSize, spaceSize, count, texturePath, disableRotate, color }: particleModelType) {
@@ -7,10 +10,10 @@ export default function Particles({ particleSize, spaceSize, count, texturePath,
     //loading texture from texture folder in public
     const texture = useTexture(texturePath || "./texture/snowflake.png")
 
+    const particleRef = useRef<THREE.Points>(null);
+
     const particleCount = count
     const pointsArray = new Float32Array(particleCount * 3)
-
-    console.log(disableRotate)
 
     for(let i = 0; i < particleCount; i++) {
         const i3 = i * 3
@@ -19,9 +22,19 @@ export default function Particles({ particleSize, spaceSize, count, texturePath,
         pointsArray[i3 + 1] = (Math.random() - 0.5) * spaceSize
         pointsArray[i3 + 2] = (Math.random() - 0.5) * spaceSize
     }
+    useFrame((state, delta) => {
+        if(!disableRotate) {
+            console.log(state)
+            if(particleRef.current) {
+                particleRef.current.rotation.x += delta - 0.02;
+                particleRef.current.rotation.y += delta - 0.02;
+            }
+
+        }
+    })
 
     return (
-        <points>
+        <points ref={particleRef} >
             <bufferGeometry>
                 <bufferAttribute
                     attach="attributes-position"
